b292e488e9dcdd2fac7e6aaef875a087b3a887b9
dxFrankenWang <149589082+dxFrankenWang@users.noreply.github.com>
2023-11-17 14:52:42 +0800

    add span.kind

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource, HOST_NAME
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import SpanKind
 
 
 def inner_method():
     tracer = trace.get_tracer(__name__)
-    with tracer.start_as_current_span("child_span"):
+    with tracer.start_as_current_span("child_span", kind=SpanKind.CLIENT):
         print("hello world")
 
 
 def outer_method():
     tracer = trace.get_tracer(__name__)
-    with tracer.start_as_current_span("parent_span"):
+    with tracer.start_as_current_span("parent_span", kind=SpanKind.SERVER):
         inner_method()
 
 
 def baggage_and_attribute_usage():
     tracer = trace.get_tracer(__name__)
     global_ctx = baggage.set_baggage("key", "value_from_global_ctx")  # 使用baggage api，在不同span之间传递数据
-    with tracer.start_as_current_span(name='baggage_parent_span', attributes={'attribute_key': 'value'}):
+    with tracer.start_as_current_span(name='baggage_parent_span', attributes={'attribute_key': 'value'},
+                                      kind=SpanKind.SERVER):
         parent_ctx = baggage.set_baggage("key", "value_from_parent_ctx")
-        with tracer.start_as_current_span(name='baggage_child_span', context=parent_ctx):
+        with tracer.start_as_current_span(name='baggage_child_span', context=parent_ctx,
+                                          kind=SpanKind.INTERNAL):
             child_ctx = baggage.set_baggage("key", "value_from_child_ctx")
 
     print(baggage.get_baggage("key", global_ctx))
@@ -35,7 +38,7 @@ def init_opentelemetry():
     resource = Resource(attributes={
         SERVICE_NAME: "PythonTest",
         HOST_NAME: "MyComputer",
-        "token": "xxxxxxxxxxx"  # 替换成控制台上的 Token
+        "token": "xxxxxxxxxx"  # 替换成控制台上的 Token
     })
 
     # 使用GRPC协议上报
